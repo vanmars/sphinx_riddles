b191525a61eb94bb519b849256f013e09da4278b
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-19 23:02:59 -0700

    Pass Spect Test 4: Create randomized roll for game play

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ namespace Spinx
   {
     public static void Main()
     {
-      // Create Instance of New Game and New Game List
+      // Create Instance of New Game, New Game List, and Random Number Generator
       RiddleGame Game = new RiddleGame();
-
       List<Dictionary<string, string>> gameList = new List<Dictionary<string, string>> {Game.Riddle1, Game.Riddle2, Game.Riddle3, Game.Riddle4, Game.Riddle5, Game.Riddle6, Game.Riddle7, Game.Riddle8};
+      System.Random random = new System.Random(); 
 
-      // Begin Game 
+      // Begining Game UI 
       Console.WriteLine("*************************************************");
       Console.WriteLine("YOU HAVE ENTERED THE LAIR OF THE SPHINX!");
       Console.WriteLine("*************************************************");
@@ -22,37 +22,44 @@ namespace Spinx
       Console.WriteLine("Answer incorrectly, . . . and you'll be devoured!");
       Console.WriteLine("*************************************************");
       
-      // Game Play Loop
-      foreach (Dictionary<string, string> riddle in gameList)
+    // Game Play Loop
+      while (gameList.Count > 0)
       {
-        Console.WriteLine("Riddle: " + riddle["riddle"]);
+        // Get Randomized Index
+        int max = gameList.Count;
+        int index = random.Next(max);
+        // Run the Write Line for the riddle at that randomized index
+        Console.WriteLine("Riddle: " + gameList[index]["riddle"]);
         string answer = Console.ReadLine();
         string answerToLower = answer.ToLower();
-        if (riddle["id"] == "8")
+        // Check if on last riddle
+        if (gameList.Count == 1)
         {
-          if (answerToLower.Contains(riddle["riddleAnswer"]))
+          if (answerToLower.Contains(gameList[index]["riddleAnswer"]))
           {
             Console.WriteLine("*** Congratulations, wise one! You have defeated the mighty Sphinx. ***");
+            gameList.RemoveAt(index);
           }
           else
           {
-            Console.WriteLine("Uh oh . . . you have answered incorrectly. You have been devoured by the Sphinx.");     
+            Console.WriteLine("Uh oh . . . you have answered incorrectly. You have been devoured by the Sphinx.");  
+            gameList.RemoveAt(index);   
          }
         }
         else
         {
-          if (answerToLower.Contains(riddle["riddleAnswer"]))
+          if (answerToLower.Contains(gameList[index]["riddleAnswer"]))
           {
             Console.WriteLine("*** Well done, you are correct. You will continue to the next round with the Spinx. ***");
+            gameList.RemoveAt(index);
           }
           else
           {
-            Console.WriteLine("Uh oh . . . you have answered incorrectly. You have been devoured by the Sphinx.");     
+            Console.WriteLine("Uh oh . . . you have answered incorrectly. You have been devoured by the Sphinx.");   
             break; 
          }
         }
       }
-      
     }
   }
 }
